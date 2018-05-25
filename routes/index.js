d2aac91d05dba4f22f4070b8f3b7d85250335657
isaacaddis <isaacaddis21@gmail.com>
2018-05-25 13:02:51 -0700

    Add Illness Route

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,6 @@
 exports.index = function(req, res){
   res.render('index', { title: "What's Eating You?" })
 };
+exports.illness = function(req, res){
+  res.render('illness', { title: "What's Eating You?" })
+};
